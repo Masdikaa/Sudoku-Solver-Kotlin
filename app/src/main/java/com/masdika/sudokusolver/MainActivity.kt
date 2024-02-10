6f8a7e78002dd755ccd4341629a576df038c59c3
Masdikaa <masdikailhanmansiz@gmail.com>
2024-02-10 23:33:50 +0700

    Adding onClick Listener

--- app/src/main/java/com/masdika/sudokusolver/MainActivity.kt
+++ app/src/main/java/com/masdika/sudokusolver/MainActivity.kt
@@ -1,13 +1,77 @@
 package com.masdika.sudokusolver
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.masdika.sudokusolver.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        binding.btnFill1.setOnClickListener(this)
+        binding.btnFill2.setOnClickListener(this)
+        binding.btnFill3.setOnClickListener(this)
+        binding.btnFill4.setOnClickListener(this)
+        binding.btnFill5.setOnClickListener(this)
+        binding.btnFill6.setOnClickListener(this)
+        binding.btnFill7.setOnClickListener(this)
+        binding.btnFill8.setOnClickListener(this)
+        binding.btnFill9.setOnClickListener(this)
+        binding.btnDelete.setOnClickListener(this)
+
     }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_fill_1 -> {
+                Toast.makeText(this, "1", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.btn_fill_2 -> {
+                Toast.makeText(this, "2", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.btn_fill_3 -> {
+                Toast.makeText(this, "3", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.btn_fill_4 -> {
+                Toast.makeText(this, "4", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.btn_fill_5 -> {
+                Toast.makeText(this, "5", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.btn_fill_6 -> {
+                Toast.makeText(this, "6", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.btn_fill_7 -> {
+                Toast.makeText(this, "7", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.btn_fill_8 -> {
+                Toast.makeText(this, "8", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.btn_fill_9 -> {
+                Toast.makeText(this, "9", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.btn_delete -> {
+                Toast.makeText(this, "Del", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
+
